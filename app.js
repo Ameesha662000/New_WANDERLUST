734d78862f52eef53438db10cc139b8166b3e1f1
Ami <117827277+Ameesha662000@users.noreply.github.com>
2024-08-31 14:11:28 +0530

    Add files

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-if (process.env.NODE_ENV != "production"){
-require('dotenv').config();
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
 }
 
 const express = require("express");
@@ -8,15 +8,15 @@ const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const ExpressError=require("./utils/ExpressError.js");
+const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
-const MongoStore = require('connect-mongo');
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
-const listingRouter = require("./routes/listing.js"); 
+const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
@@ -31,37 +31,36 @@ main()
   });
 
 async function main() {
-  await mongoose.connect(dbUrl); 
+  await mongoose.connect(dbUrl);
 }
-   
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true })); //jisse humara sara data jo req me aarha shi se parse ho paye
 app.use(methodOverride("_method"));
-app.engine('ejs', ejsMate);
+app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
-
 
 const store = MongoStore.create({
   mongoUrl: dbUrl,
-  crypto:{
-    secret:process.env.SECRET,
+  crypto: {
+    secret: process.env.SECRET,
   },
-  touchAfter:24 * 3600,
+  touchAfter: 24 * 3600,
 });
 
-store.on("error",()=>{
+store.on("error", () => {
   console.log("Error in MONGO SESSION STORE", err);
 });
 
 const sessionOptions = {
   store,
-  secret :process.env.SECRET,
-  resave : false,
+  secret: process.env.SECRET,
+  resave: false,
   saveUninitialized: true,
-  cookie:{
-    expires: Date.now() + 7 * 24 *60 *60 *1000,
-    maxAge:7 * 24 *60 *60 *1000,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
   },
 };
@@ -69,9 +68,6 @@ const sessionOptions = {
 // app.get("/", (req, res) => {
 //   res.send("Hi, I am root");
 // });
-
-
-
 
 app.use(session(sessionOptions));
 app.use(flash());
@@ -83,26 +79,25 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req,res, next)=>{
+app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   res.locals.currUser = req.user;
   next();
 });
 
-
 // app.get("/demouser", async (req,res)=>{
 //    let fakeUser = new User({
 //     email: "student@gmail.com",
 //     username:"delta-student"
 //    });
-//   let registeredUser =await User.register(fakeUser, "helloworlsd"); 
+//   let registeredUser =await User.register(fakeUser, "helloworlsd");
 //   res.send(registeredUser);
 // })
 
-app.use("/listings",listingRouter);
-app.use("/listings/:id/reviews",reviewRouter);
-app.use("/",userRouter);
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 // app.get("/testListing", async (req, res) => {
 //   let sampleListing = new Listing({
@@ -118,15 +113,14 @@ app.use("/",userRouter);
 //   res.send("successful testing");
 // });
 
-app.all("*",(req,res,next)=>{
+app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page Not Found"));
 });
 
-app.use((err,req,res,next)=>{
-  let {statusCode=500 ,message="Something went wrong"}=err;
-  res.status(statusCode).render("error.ejs",{message});
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something went wrong" } = err;
+  res.status(statusCode).render("error.ejs", { message });
   //res.status(statusCode).send(message);
-  
 });
 
 app.listen(8080, () => {
